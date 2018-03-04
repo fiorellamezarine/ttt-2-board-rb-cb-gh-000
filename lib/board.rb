16d3a3e0011a546b42e037faef400a40d55a287a
Fiorella Mezarine <nekili4@yahoo.com>
2018-03-04 23:14:39 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 board = Array
-board = ["top_left", "top_middle", "top_right", "center_left", "center_middle", "center_right", "bottom_left", "bottom_middle", "bottom_right"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
